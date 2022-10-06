b90c2936428e3bf2dd0120570dd2e50ab9d31a89
ikarodvd <ikaromiguel13@gmail.com>
2022-10-06 00:21:24 -0400

    feat: adicionando e relacionando modelos

--- survey/models.py
+++ survey/models.py
@@ -1,15 +1,28 @@
+from multiprocessing.connection import answer_challenge
 from django.db import models
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+class Survey(models.Model):
+    title = models.CharField(max_length=200, primary_key = True)
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return self.question_text
+        return self.title
     
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class SurveyQuestion(models.Model):
+    question = models.CharField(max_length=200)
+    surveyTitle = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    #choice_text = models.CharField(max_length=200)
     def __str__(self):
-        return self.choice_text
+        return self.question
+
+class SurveyQuestionAlternative(models.Model):
+    questionAlternative = models.CharField(max_length=200)
+    surveyQuestion = models.ForeignKey(SurveyQuestion, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.questionAlternative
+
+class SurveyUserAnswer(models.Model):
+    surveyQuestionAlternative = models.ForeignKey(SurveyQuestionAlternative, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=200)
+    def __str__(self):
+        return self.answer
